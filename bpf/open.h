740b4393859bb2398e06362d49e19a1811468a1f
John Tyler <jetyler@ualberta.ca>
2026-04-18 06:44:32 -0600

    update open.h header with resolve values for openat2 syscall

--- bpf/open.h
+++ bpf/open.h
@@ -47,4 +47,12 @@
 #define S_IWOTH 02
 #define S_IXOTH 01
 
+// resolve flags for openat2(2)
+#define RESOLVE_NO_XDEV       0x01
+#define RESOLVE_NO_MAGICLINKS 0x02
+#define RESOLVE_NO_SYMLINKS   0x04
+#define RESOLVE_BENEATH       0x08
+#define RESOLVE_IN_ROOT       0x10
+#define RESOLVE_CACHED        0x20
+
 #endif
